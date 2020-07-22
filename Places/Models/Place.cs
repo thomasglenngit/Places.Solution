163287662059e9c1ff5f30fa8e43c937edc7b66b
Allison Sadin <aesadin@gmail.com>
2020-07-22 13:18:09 -0700

    add methods to view list, clear list and search list for destination Id
    Co-authored-by: Thomas Glenn <glenaissance@yahoo.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>

---
+++ Places/Models/Place.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace Places.Models
+{
+    public class Place
+    {
+      public string City { get; set; }
+      public string Food { get; set; }
+
+      // public string Photo { get; set; }
+      Image newImage = Image.FromFile("img_submit.gif");
+      public int Id { get; }
+      private static List<Place> _listOfDestinations = new List<Place> {};
+      
+      public Place(string city, string food, image photo)
+      {
+        City = city;
+        Food = food;
+        Photo = photo;
+        __listOfDestinations.Add(this);
+        Id = _listOfDestinations.Count;
+
+      }
+
+      public static List<Place> GetAll()
+      {
+        return _listOfDestinations;
+      }
+
+      public static void ClearAll()
+      {
+        _listOfDestinations.Clear();
+      }
+
+      public static Place Find(int searchId)
+      {
+        return _listOfDestinations[searchId - 1];
+      }
+    }
+}
