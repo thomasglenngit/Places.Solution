4a6959f53ad1be3fa2fe42ca2cd0167978a6fb88
Allison Sadin <aesadin@gmail.com>
2020-07-22 14:11:47 -0700

    passed first constructor test
    Co-authored-by: Thomas Glenn <glenaissance@yahoo.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>

--- Places.Tests/ModelTests/PlaceTests.cs
+++ Places.Tests/ModelTests/PlaceTests.cs
@@ -11,8 +11,22 @@ namespace Places.Tests
     [TestMethod]
     public void PlacesConstructor_CreatesAnInstanceOfAnObjectUsingConstructor_Place()
     {
-      Place newPlace = new Place("test", "test");
+      Place newPlace = new Place("test", "testy");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsTheListOfDestinations_List()
+    {
+      string city = "Tokyo";
+      string food = "Katsu";
+      Place newPlace = new Place(city, food);
+      List<Place> newList = new List<Place> {newPlace};
+
+      List<Place> result = Place.GetAll();
+      
+      CollectionAssert.AreEqual(newList, result);
+
     }
   }
 }
--- Places/Models/Place.cs
+++ Places/Models/Place.cs
@@ -2,30 +2,30 @@ using System.Collections.Generic;
 
 namespace Places.Models
 {
-    public class Place
+    public class Place 
     {
       public string City { get; set; }
       public string Food { get; set; }
 
-      // public string Photo { get; set; }
+      public string Photo { get; set; }
       // Image newImage = Image.FromFile("img_submit.gif");
       public int Id { get; }
       private static List<Place> _listOfDestinations = new List<Place> {};
       
-      public Place()
+      public Place(string city, string food)
       {
-        // City = city;
-        // Food = food;
+        City = city;
+        Food = food;
         // Photo = photo;
         // __listOfDestinations.Add(this);
-        // Id = _listOfDestinations.Count;
+        Id = _listOfDestinations.Count;
 
       }
 
-      // public static List<Place> GetAll()
-      // {
-      //   return _listOfDestinations;
-      // }
+      public static List<Place> GetAll()
+      {
+        return _listOfDestinations;
+      }
 
       // public static void ClearAll()
       // {
